2b2cfdf38fa48e3ae22e3b4f39ff380e8e4f54ef
yindaheng98 <yindaheng98@gmail.com>
2025-11-29 16:32:22 -0800

    better logic

--- paper_weaver/dataclass.py
+++ paper_weaver/dataclass.py
@@ -11,8 +11,8 @@ class Paper:
     async def get_authors(self, src: "DataSrc") -> list["Author"]:
         return await src.get_authors_by_paper(self)
 
-    async def get_venue(self, src: "DataSrc") -> list["Venue"]:
-        return await src.get_venue_by_paper(self)
+    async def get_venues(self, src: "DataSrc") -> list["Venue"]:
+        return await src.get_venues_by_paper(self)
 
     async def get_references(self, src: "DataSrc") -> list["Paper"]:
         return await src.get_references_by_paper(self)
@@ -55,7 +55,7 @@ class DataSrc(metaclass=ABCMeta):
         raise NotImplementedError
 
     @abstractmethod
-    async def get_venue_by_paper(self, paper: Paper) -> list[Venue]:
+    async def get_venues_by_paper(self, paper: Paper) -> list[Venue]:
         raise NotImplementedError
 
     @abstractmethod
@@ -65,3 +65,53 @@ class DataSrc(metaclass=ABCMeta):
     @abstractmethod
     async def get_citations_by_paper(self, paper: Paper) -> list[Paper]:
         raise NotImplementedError
+
+
+class DataDst(metaclass=ABCMeta):
+    @abstractmethod
+    async def save_paper(self, paper: Paper) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def link_citation(self, paper: Paper, citation: Paper) -> None:
+        raise NotImplementedError
+
+    async def link_citations(self, paper: Paper, citations: list[Paper]) -> None:
+        """Overwrite batch link citations for better performance."""
+        for citation in citations:
+            await self.link_citation(paper, citation)
+
+    @abstractmethod
+    async def link_reference(self, paper: Paper, reference: Paper) -> None:
+        raise NotImplementedError
+
+    async def link_references(self, paper: Paper, references: list[Paper]) -> None:
+        """Overwrite batch link references for better performance."""
+        for reference in references:
+            await self.link_reference(paper, reference)
+
+    @abstractmethod
+    async def save_author(self, author: Author) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def link_author(self, paper: Paper, author: Author) -> None:
+        raise NotImplementedError
+
+    async def link_authors(self, paper: Paper, authors: list[Author]) -> None:
+        """Overwrite batch link authors for better performance."""
+        for author in authors:
+            await self.link_author(paper, author)
+
+    @abstractmethod
+    async def save_venue(self, venue: Venue) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def link_venue(self, paper: Paper, venue: Venue) -> None:
+        raise NotImplementedError
+
+    async def link_venues(self, paper: Paper, venues: list[Venue]) -> None:
+        """Overwrite batch link venues for better performance."""
+        for venue in venues:
+            await self.link_venue(paper, venue)
