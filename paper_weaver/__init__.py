840e98f97a0fe618a0abed3bb3c8063a63784e7e
yindaheng98 <yindaheng98@gmail.com>
2025-11-30 16:55:55 -0800

    split

--- paper_weaver/__init__.py
+++ paper_weaver/__init__.py
@@ -1,2 +1,4 @@
 from .dataclass import Paper, Author, Venue, DataSrc, DataDst
-from .iface import AuthorWeaver, AuthorWeaverCache
+from .iface import WeaverIface, Author2PapersWeaverIface, Paper2AuthorsWeaverIface
+from .iface import Author2PapersWeaverCacheIface, Paper2AuthorsWeaverCacheIface
+from .authorweaver import AuthorWeaver, AuthorWeaverCache
--- paper_weaver/iface.py
+++ paper_weaver/iface.py
@@ -1,7 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import asyncio
-import logging
-from typing import AsyncIterator, Tuple
+from typing import Tuple
 from .dataclass import Paper, Author, DataSrc, DataDst
 
 
@@ -198,54 +197,3 @@ class Paper2AuthorsWeaverIface(WeaverIface, metaclass=ABCMeta):
         n_failed = sum([1 for r in results if r is None])
 
         return n_new_authors, n_failed
-
-
-class AuthorWeaverCache(Author2PapersWeaverCacheIface, Paper2AuthorsWeaverCacheIface, metaclass=ABCMeta):
-
-    @abstractmethod
-    def iterate_authors(self) -> AsyncIterator[Author, None]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def iterate_papers(self) -> AsyncIterator[Paper, None]:
-        raise NotImplementedError
-
-
-class AuthorWeaver(Author2PapersWeaverIface, Paper2AuthorsWeaverIface):
-    logger = logging.getLogger("AuthorWeaver")
-
-    def __init__(self, src: DataSrc, dst: DataDst, cache: AuthorWeaverCache):
-        self._src = src
-        self._dst = dst
-        self._cache = cache
-
-    @property
-    def src(self) -> DataSrc:
-        return self._src
-
-    @property
-    def dst(self) -> DataDst:
-        return self._dst
-
-    @property
-    def cache(self) -> AuthorWeaverCache:
-        return self._cache
-
-    async def bfs_once(self):
-        tasks = []
-        async for author in self.cache.iterate_authors():
-            tasks.append(self.author_to_papers(author))
-        self.logger.info(f"Fetching papers from {len(tasks)} new authors")
-        state = await asyncio.gather(*tasks)
-        author_succ_count, author_fail_count = sum([1 for s in state if s is not None]), sum([1 for s in state if s is None])
-        paper_succ_count, paper_fail_count = sum([s[0] for s in state if s is not None]), sum([s[1] for s in state if s is not None])
-        self.logger.info(f"Found {paper_succ_count} new papers from {author_succ_count} authors. {paper_fail_count} papers fetch failed. {author_fail_count} authors fetch failed.")
-
-        tasks = []
-        async for paper in self.cache.iterate_papers():
-            tasks.append(self.paper_to_authors(paper))
-        self.logger.info(f"Fetching authors from {len(tasks)} new papers")
-        state = await asyncio.gather(*tasks)
-        paper_succ_count, paper_fail_count = sum([1 for s in state if s is not None]), sum([1 for s in state if s is None])
-        author_succ_count, author_fail_count = sum([s[0] for s in state if s is not None]), sum([s[1] for s in state if s is not None])
-        self.logger.info(f"Found {author_succ_count} new authors from {paper_succ_count} papers. {author_fail_count} authors fetch failed. {paper_fail_count} papers fetch failed.")
