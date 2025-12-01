db39fc350104aaf0bd2e7e4cb3707e7c676f9bd9
yindaheng98 <yindaheng98@gmail.com>
2025-11-30 17:24:09 -0800

    PaperLinkWeaverCacheIface

--- paper_weaver/iface_link.py
+++ paper_weaver/iface_link.py
@@ -12,3 +12,20 @@ class AuthorLinkWeaverCacheIface(WeaverCacheIface, metaclass=ABCMeta):
     @abstractmethod
     async def link_author(self, paper: Paper, author: Author) -> None:
         raise NotImplementedError
+
+
+class PaperLinkWeaverCacheIface(WeaverCacheIface, metaclass=ABCMeta):
+
+    @abstractmethod
+    async def is_link_reference(self, paper: Paper, reference: Paper) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def link_reference(self, paper: Paper, reference: Paper) -> None:
+        raise NotImplementedError
+
+    async def is_link_citation(self, paper: Paper, citation: Paper) -> bool:
+        return await self.is_link_reference(citation, paper)
+
+    async def link_citation(self, paper: Paper, citation: Paper) -> None:
+        return await self.link_reference(citation, paper)
