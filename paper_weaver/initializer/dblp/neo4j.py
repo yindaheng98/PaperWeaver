ec05150eb1f7ce93a3835f373ea982398464f47c
yindaheng98 <yindaheng98@gmail.com>
2025-12-04 20:52:15 -0800

    # TODO: Load DBLP from Neo4J

---
+++ paper_weaver/initializer/dblp/neo4j.py
@@ -0,0 +1,22 @@
+from typing import AsyncIterator
+
+from ...dataclass import Paper, Author, Venue
+from ...iface_init import (
+    PapersWeaverInitializerIface,
+    AuthorsWeaverInitializerIface,
+    VenuesWeaverInitializerIface,
+)
+
+
+class Neo4JDBLPPapersInitializer(PapersWeaverInitializerIface):
+    pass
+
+
+class Neo4JDBLPAuthorsInitializer(AuthorsWeaverInitializerIface):
+    pass
+
+
+class Neo4JDBLPVenuesInitializer(VenuesWeaverInitializerIface):
+    pass
+
+# TODO: Load from Neo4J
